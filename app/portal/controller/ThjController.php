727313ba7a3f78f3dd5fc9fdeaf34670f67cdcc6
11223ren <1132974092@qq.com>
2018-07-12 10:03:26 +0800

    是

---
+++ app/portal/controller/ThjController.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace app\portal\controller;
+
+use cmf\controller\HomeBaseController;
+
+class ThjController extends HomeBaseController
+{
+    public function th()
+    {
+       exit('index');
+    }
+}
